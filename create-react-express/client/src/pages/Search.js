7c90521662eda6b3a478da6b4ce54d41fa7e309e
Little Milo <no-reply>
2019-07-21 16:58:09 -0700

    React Router displays /Saved && /Search pages

---
+++ create-react-express/client/src/pages/Search.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+class Search extends Component {
+  render () {
+    return (
+      <h1>SEARCH PAGE_----------------------------</h1>
+    )
+  }
+}
+export default Search
