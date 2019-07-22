a2e77b1f11b10905425751dde65f2a5afb27144b
Little Milo <no-reply>
2019-07-21 19:10:02 -0700

    didn't have bootstrap npm installed or linked, issue fixed after 3 hours

--- create-react-express/client/src/pages/Search.js
+++ create-react-express/client/src/pages/Search.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
 
 class Search extends Component {
   render () {
     return (
-      <h1>SEARCH PAGE_----------------------------</h1>
+      // <h1>TEST</h1>
+      <Navbar bg='primary' variant='dark'>
+        <Navbar.Brand href='/'>Googlebooks</Navbar.Brand>
+        <Nav className='mr-auto'>
+          <Nav.Link href='/Search'>Search</Nav.Link>
+          <Nav.Link href='/Saved'>Saved</Nav.Link>
+        </Nav>
+      </Navbar>
     )
   }
 }
